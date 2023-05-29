ffc566e65d365ce36d9d2a2d76f965b1ddf8c141
Alexey Knyazev <bsagdiev74@gmail.com>
2023-05-29 18:28:05 +0300

    Добавили стили

--- src/Components/Posts/PostCard/index.tsx
+++ src/Components/Posts/PostCard/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import styles from './PostCard.module.scss';
 
 export const PostCard = () => {
-  return <div>PostCard</div>
+  return <div className={styles.cardWrapper}>
+    <span className={styles.label}>Post Label</span>
+    <p className={styles.cotent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus deserunt quo tenetur atque illum, vitae obcaecati nostrum, non vero facere minima ipsam dignissimos quisquam cumque sunt voluptates consequuntur impedit adipisci.</p>
+    </div>
 };
