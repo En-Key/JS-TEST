42ebd399fa4c8fe691ff7efe42f66d27b35468cc
Alexey Knyazev <bsagdiev74@gmail.com>
2023-05-29 19:43:32 +0300

    Добавлены ссылки линк

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { MainPage } from './Containers/MainPage';
 import { PostPage } from './Containers/PostPage';
 import styles from './App.module.scss'
+import { Route, Routes } from 'react-router-dom';
+import { Page } from './Containers/Page';
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      <MainPage />
-      {/* <PostPage /> */}
+      <Routes>
+        <Route path="/" element={<MainPage />}/>
+        <Route path="/posts">
+          <Route element={<PostPage />} index />
+          <Route path="one-post" element={<Page />} />
+          </Route>
+        <Route />
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// const h1 = React.createElement('h1', { className: 'test' }, 'Hello World');
-// const div = React.createElement('div', { style: { width: '200px', height: '300px', backgroundColor: 'black' }, },
-//   h1,
-// )
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  //   {div}
-  // </React.StrictMode>
+
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
