8919ea95f47b71da3597078c720857a0c7b5fc0e
Alexey Knyazev <bsagdiev74@gmail.com>
2023-05-29 13:02:15 +0300

    Несколько страниц

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
-import React, { useState } from 'react';
-import './App.css';
-import { ProductCard } from './ProductCard';
-import { CustomInput } from './CustomInput';
-
-const productArray = [{
-  id: 1,
-  name: 'Apple',
-},
-{
-  id: 2,
-  name: 'Orange',
-},
-{
-  id: 3,
-  name: 'Tomato',
-},
-];
+import React from 'react';
+import { MainPage } from './Containers/MainPage';
+import { PostPage } from './Containers/PostPage';
+import styles from './App.module.css'
 
 function App() {
-  // const buttonHandler = () => { console.log('hi'); };
-
-  const [limit, setLimit] = useState(10);
-
-  const addLimit = () => {
-    setLimit(limit + 1);
-  };
-
-  const deleteLimit = () => {
-    setLimit(limit - 1);
-  };
-
   return (
-    <div className="App">
-      <CustomInput/>
-      <button onClick={deleteLimit} disabled={limit === 0}>delete</button>
-        <span>{limit}</span>
-        <button onClick={addLimit}>add</button>
-      {/* <ProductCard productName='Orange'/> */}
-      {productArray.map(({ id, name }) => (
-      <ProductCard productName={name} key={id} limit={limit}/>
-      ))}
+    <div className={styles.wrapper}>
+      {/* <MainPage /> */}
+      <PostPage />
     </div>
   );
 }
---
+++ src/Containers/PostPage/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header } from '../../Components/Commons/Header';
+import { Footer } from '../../Components/Commons/Footer';
+import { PostCard } from '../../Components/Posts/PostCard';
+import styles from './PostPage.module.css';
+
+export const PostPage = () => {
+  return <div className={styles.postPageWrapper}>
+    <Header />
+    <div className={styles.postList}>
+      {/* <span>PostPage</span> */}
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    </div>
+    <Footer />
+    </div>
+};
