bbbd0ce157c38c17e74db1b04b0557a43fda7682
Alexey Knyazev <bsagdiev74@gmail.com>
2023-06-01 17:48:40 +0300

    add store

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/configureStore';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +14,9 @@ root.render(
 
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
